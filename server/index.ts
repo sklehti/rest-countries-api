2a3ee055c28e760abdde2042dfc809a347d2dcb6
Saara Lehtinen <saaralehtinen77@gmail.com>
2023-09-18 16:15:48 +0300

    one bux fixed

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import countryRouter from "./routes/countries";
+import * as url from "url";
 
 const app = express();
 app.use(cors());
@@ -10,11 +11,20 @@ app.use(express.static("build"));
 
 app.use("/api/countries", countryRouter);
 
+const PORT = process.env.PORT || 3001;
+
+app.get("/*", (req, res) => {
+  res.redirect(
+    url.format({
+      protocol: req.protocol,
+      host: req.get("host"),
+    })
+  );
+});
+
 app.get("/test", (_req, res) => {
   res.send("The test works correctly.");
 });
-
-const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
