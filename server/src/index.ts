8a62dc93f454da3385d296083370040f841a99e3
Saara Lehtinen <saaralehtinen77@gmail.com>
2023-09-19 09:51:06 +0300

    server side index.ts updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import countryRouter from "./routes/countries";
+import * as url from "url";
 
 const app = express();
 app.use(cors());
@@ -9,6 +10,21 @@ app.use(express.json());
 app.use(express.static("./build"));
 
 app.use("/api/countries", countryRouter);
+
+// Note! You have to be sure ip address is trusted
+app.set("trust proxy", function (ip: string) {
+  if (ip === "127.0.0.1") return false;
+  else return true;
+});
+
+app.get("/*", (req, res) => {
+  res.redirect(
+    url.format({
+      protocol: req.protocol,
+      host: req.get("host"),
+    })
+  );
+});
 
 app.get("/test", (_req, res) => {
   res.send("The test works correctly.");
